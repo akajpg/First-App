7762e6979f730953f4c67fc52c4abfba63c18df5
JPG <akajpg@me.com>
2025-04-18 21:59:26 -0700

    avatar added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -21,12 +21,10 @@ export default function Index() {
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
-        <Image
-          source={{
-            uri: "https://images.unsplash.com/photo-1519455953755-af066f52f1a6?q=80&w=2261&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          }}
-          style={styles.image} 
-        />
+      <Image
+        source={require('../../assets/images/avatar.jpg')}
+        style={{ width: 200, height: 200, borderRadius: 100 }}
+      />
 
         {/* Typography */}
         <Text style={typography.h1}>Heading 1</Text>
